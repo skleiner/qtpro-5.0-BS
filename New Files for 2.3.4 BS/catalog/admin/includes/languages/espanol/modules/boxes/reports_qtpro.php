44851bc116b6c38a2379b2347204c104058eedce
Lionel <admin@nukua.es>
2017-01-21 23:05:24 +0100

    Update reports_qtpro.php

--- New Files for 2.3.4 BS/catalog/admin/includes/languages/espanol/modules/boxes/reports_qtpro.php
+++ New Files for 2.3.4 BS/catalog/admin/includes/languages/espanol/modules/boxes/reports_qtpro.php
@@ -10,4 +10,4 @@
   Released under the GNU General Public License
 */
 
-  define('MODULES_ADMIN_MENU_REPORTS_STATS_LOW_STOCK_ATTRIB', 'Stock Report');
+  define('MODULES_ADMIN_MENU_REPORTS_STATS_LOW_STOCK_ATTRIB', 'Informe de Stock');
