f29cc43de9eb47506cef4d0e6309d48a3bf4c889
raiwa <info@sarplataygemas.com>
2017-01-22 12:08:38 +0100

    update language file admin/stock.php
    - added missing definitions to admin/stock.php language files
    - removed swedish language files

--- New Files for 2.3.4 BS/catalog/admin/includes/languages/german/stock.php
+++ New Files for 2.3.4 BS/catalog/admin/includes/languages/german/stock.php
@@ -40,5 +40,24 @@ define('WARNING_NO_PRODUCT','Warning! This product does not seem to exist in any
 define('BUTTON_ADD','Add');
 define('BUTTON_UPDATE','Update');
 
-
-?>
+// detailed product inverstigation used in qtpro_doctor_formulate_product_investigation function
+define('TEXT_DETAILED_STOCK_MATCH_TRUE','<span style="color:green; font-weight: bold; font-size:1.2em;">The stock quantity summary is ok</span><br>
+				This means that the current summary of this products quantity, which is in the database, is the value we get if we calculates it from scratch right now.<br>
+				<b>The Summary stock is: %s </b><br><br>');
+define('TEXT_DETAILED_STOCK_MATCH_FALSE','<span style="color:red; font-weight: bold; font-size:1.2em;">The stock quantity summary is NOT ok</span><br>
+				This means that the current summary of this products quantity, which is in the database, isn\'t the value we get if we calculates it from scratch right now.<br>
+				<b>The current summary stock is: %s </b><br>
+				<b>If we calculates it we get: %s </b><br><br>');
+define('TEXT_DETAILED_STOCK_ENTRIES_HEALTHY','<span style="color:green; font-weight: bold; font-size:1.2em;">The options stock is ok</span><br>
+				This means that the database entries for this product looks the way they should. No options are missing in any row. No option exist in any row where it should not.<br>
+				<b>Total number of stock entries this product has: %s </b><br>
+				<b>Number of messy entries: %s </b><br>');
+define('TEXT_DETAILED_STOCK_ENTRIES_NOT_HEALTHY','<span style="color:red; font-weight: bold; font-size:1.2em;">The options stock is NOT ok</span><br>
+				This means that at least one of the database entries for this product is messed up. Either options are missing in rows or options exist in rows they should not.<br>
+				<b>Total number of stock entries this product has: &s </b><br>
+				<b>Number of messy entries: %s</b><br><br>');
+define('TEXT_DETAILED_STOCK_SOLUTIONS_STOP_TRACKING','<span style="color:blue; font-weight: bold;">Possible solutions: </span>Delete the corresponding row(s) from the database or stop tracking the stock for that option.<br><br>');
+define('TEXT_DETAILED_STOCK_OPTIONS_SHOULD_NOT_EXIST','<br><b>These options exists in row(s) although they should not:</b><br>');
+define('TEXT_DETAILED_STOCK_SOLUTIONS_START_TRACKING','<span style="color:blue; font-weight: bold;">Possible solutions: </span>Delete the corresponding row(s) from the database or start tracking the stock for that option.<br><br>');
+define('TEXT_DETAILED_STOCK_LINK_AMPUTATION','Amputation (Deletes all messy rows)');
+define('TEXT_DETAILED_STOCK_LINK_SET_SUMMARY','Set the summary stock to: %s');
